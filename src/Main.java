fb18a3ddc15b2034b73d401a3206e702a338b0ae
jhwan <think2008@nate.com>
2024-04-26 17:16:40 +0900

    암호만들기

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,12 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
+        char[] helo = { 'a', 'b', 'C', 'd'};
+        String[] test = {"hello", " my name is ", "what ? "};
 
-        StringBuffer sb = new StringBuffer();
-        sb.append(a*(b%10) + "\n");
-        sb.append(a*((b%100)/10) + "\n");
-        sb.append(a*(b/100) + "\n");
-        sb.append(a*b);
+        System.out.println(helo);
 
-        System.out.println(sb);
+        System.out.println(test.toString());
     }
 }
